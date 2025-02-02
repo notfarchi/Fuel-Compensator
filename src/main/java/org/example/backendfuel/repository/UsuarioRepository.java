cc3658388fac40c15635ba8900b085ae22b51345
João Victor Farchi <91800826+notfarchi@users.noreply.github.com>
2025-02-02 10:07:36 -0300

    Delete src directory

--- src/main/java/org/example/backendfuel/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backendfuel.repository;
-
-import org.example.backendfuel.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
